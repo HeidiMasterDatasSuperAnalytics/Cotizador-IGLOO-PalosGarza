87d443ca1eb49d4fe8cba60257879fb05e26de38
HeidiMasterDatasSuperAnalytics <heidi.31032003@gmail.com>
2025-04-25 17:53:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
 import streamlit as st
 
-# Centrado y grande
-st.image("Igloo Original.png", width=300)
+st.markdown("""
+    <style>
+    .logo-img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 300px;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .logo-light { display: none; }
+        .logo-dark { display: block; }
+    }
+
+    @media (prefers-color-scheme: light) {
+        .logo-light { display: block; }
+        .logo-dark { display: none; }
+    }
+    </style>
+
+    <div>
+        <img src="Igloo Original.png" class="logo-img logo-light">
+        <img src="Igloo White.png" class="logo-img logo-dark">
+    </div>
+""", unsafe_allow_html=True)
 
 st.markdown("""
 # Bienvenido al Cotizador de Rutas 🚛📊
